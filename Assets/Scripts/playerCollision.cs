cc7ea1c047831e734c8bc790153683d86ea600c5
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-05-26 12:09:42 -0300

    movement & trapIns

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.name == "DeathWall")
+        {
+            //Destroy(gameObject);
+            transform.position = new Vector3(0, 0.6f, 0);
+        }
+
+        if (col.gameObject.name == "caida")
+        {
+            transform.position = new Vector3(0, 0.6f, 0);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public GameObject trap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject trapClon = Instantiate(trap);
+            Destroy(trapClon, 2);
+            
+            
+        }
+
+    }
+}
